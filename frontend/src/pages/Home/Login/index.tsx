dc21482608dd02e61f4457316b94eff4d28f3647
eliezerrb <eliezerrezende@outlook.com>
2023-05-09 08:22:46 -0300

    Validação dos campos de login

--- frontend/src/pages/Home/Login/index.tsx
+++ frontend/src/pages/Home/Login/index.tsx
@@ -1,5 +1,6 @@
 import './styles.css';
 import Button from 'components/Button';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { requestBackendLogin } from 'util/requests';
 
@@ -9,46 +10,70 @@ type FormData = {
 };
 
 const Login = () => {
+  const [hasError, setHasError] = useState(false);
 
   const {
     register,
     handleSubmit,
+    formState: { errors },
   } = useForm<FormData>();
 
-  const onSubmit = (formData : FormData) => {
+  const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
-    .then(response => {
-      console.log('SUCESSO', response);
-    })
-    .catch(error => {
-      console.log('ERRO', error);
-    });
+      .then((response) => {
+        setHasError(false);
+        console.log('SUCESSO', response);
+      })
+      .catch((error) => {
+        setHasError(true);
+        console.log('ERRO', error);
+      });
   };
-  
 
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
+      )}
       <form onSubmit={handleSubmit(onSubmit)}>
         {/*mb-4: Classe bootStrap margin botton 4 espaço abaixo para outro imput*/}
         {/*form-control: Classe bootStrap que coloca borda no imput, largura 100% */}
         <div className="mb-4">
           <input
-            {...register("username")}
+            {...register('username', {
+              required: 'Campo obrigatório',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email inválido',
+              },
+            })}
             type="text"
-            className="form-control base-input"
+            className={`form-control base-input ${
+              errors.username ? 'is-invalid' : ''
+            }`}
             placeholder="Email"
             name="username"
           />
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
         <div className="mb-5">
           <input
-            {...register("password")}
+            {...register('password', {
+              required: 'Campo obrigatório',
+            })}
             type="password"
-            className="form-control base-input "
+            className={`form-control base-input ${
+              errors.password ? 'is-invalid' : ''
+            }`}
             placeholder="Password"
             name="password"
           />
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
         <div className="login-submit">
           <Button text="Fazer login" />
