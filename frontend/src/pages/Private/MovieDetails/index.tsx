43e9ba8fc07401b917a820639a8066ccbceddfc2
eliezerrb <eliezerrezende@outlook.com>
2023-07-09 17:32:53 -0300

    finalizando página de detalhes do filme com avaliação

--- frontend/src/pages/Private/MovieDetails/index.tsx
+++ frontend/src/pages/Private/MovieDetails/index.tsx
@@ -7,6 +7,7 @@ import { Review } from 'types/reviews';
 import ReviewForm from 'components/ReviewForm';
 import { hasAnyRoles } from 'util/auth';
 import ReviewList from 'components/ReviewList';
+import { Movie } from 'types/movie';
 
 type urlParams = {
   movieId: string;
@@ -16,6 +17,8 @@ const MovieDetails = () => {
   const { movieId } = useParams<urlParams>();
 
   const [reviews, setReviews] = useState<Review[]>([]);
+
+  const [movie, setMovie] = useState<Movie>();
 
   useEffect(() => {
     const config: AxiosRequestConfig = {
@@ -30,21 +33,52 @@ const MovieDetails = () => {
     });
   }, [movieId]);
 
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: `/movies/${movieId}`,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then((response) => {
+      // Atribuindo o resultado do response para o Movie com a função setMovie
+      setMovie(response.data);
+    });
+  }, [movieId]);
 
   // Para funcionar quando salva uma nova avaliação renderiza o componente novamente
   const handleInsertReview = (review: Review) => {
     const clone = [...reviews];
     clone.push(review);
     setReviews(clone);
-  }
+  };
 
   return (
     <div className="moviedetails-container">
-      <div className="moviedetails-content-container">
-        <h1>Tela detalhes do filme id: {movieId}</h1>
+      <div className="base-card moviedetails-content-container">
+        <div className="row">
+          <div className="col-xl-6">
+            <img
+              className="moviedetails-img"
+              src={movie?.imgUrl}
+              alt={movie?.title}
+            />
+          </div>
+          <div className="col-xl-6 moviedetails-description">
+            <h2>{movie?.title}</h2>
+            <h3>{movie?.year}</h3>
+            <h4>{movie?.subTitle}</h4>
+            <div className="moviedetails-synopsis">
+              <h5>{movie?.synopsis}</h5>
+            </div>
+          </div>
+        </div>
       </div>
-      {hasAnyRoles(['ROLE_MEMBER']) && <ReviewForm movieId={movieId} onInsertReview={handleInsertReview} />}
-      
+
+      {hasAnyRoles(['ROLE_MEMBER']) && (
+        <ReviewForm movieId={movieId} onInsertReview={handleInsertReview} />
+      )}
+
       <ReviewList reviews={reviews} />
     </div>
   );
